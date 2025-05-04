b9e42e67b80a6590fba14b8fbfeaae647e9b153d
LordofInverness <jb007isboss@gmail.com>
2025-05-03 22:16:26 -0400

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -1,1 +1,22 @@
-
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+#include <iostream>
+using  namespace std;
+template <typename T >
+class Linkedlist {
+private:
+  struct Node {
+    T data;
+    Node* next;
+    Node(const T& val ) : data(val), next(nullptr) {} };
+    Node* head;
+public:
+Linkedlist();
+~Linkedlist();
+void push_front(const T& value);
+void pop_front();
+void display() const;
+bool is_empty()const;
+int size()const;
+};
+#endif
